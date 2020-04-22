28096a769627d92d35daf47500eaaf67d618658a
Takahashi Noriyuki <takahashi-n@chwd.ch-mk.jp>
2020-04-22 17:26:36 +0900

    first

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+export default () =><div>
+  <style jsx>{`
+  h1 {
+    font-size: 68pt;
+    font-weight: bold;
+    text-align: right;
+    letter-spacing: -8px;
+    color: #f0f0f0;
+    margin:-32px 0px;
+  }
+  p {
+    margin: 0px;
+    color:#666;
+    font-size: 16pt;
+  }
+  `}</style>
+  <h1>Next.js</h1>
+  <p>Welcome to next.js:</p>
+</div>
