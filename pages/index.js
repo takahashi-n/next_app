1c79c3b493d97e882ecfa54d11a883dc143a96a9
Takahashi Noriyuki <takahashi-n@chwd.ch-mk.jp>
2020-04-23 15:46:11 +0900

    reduxデータ共有

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
 import Link from 'next/link';
 import Layout from '../components/Layout';
-import Image from '../static/Image';
+import Counter from '../components/Counter';
 
 export default () =>(
   <Layout header="Next" title="Top page.">
-    <p>Welcome to next.js:</p>
-    <Image fname="image.png" size="100" />
+    <Counter />
     <hr />
     <Link href="/other">
       <button>Go to Other page &gt;&gt;</button>
