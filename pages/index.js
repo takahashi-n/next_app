eb09074ad326c9ead09fcf728d74d48f8174e770
Takahashi Noriyuki <takahashi-n@chwd.ch-mk.jp>
2020-04-23 14:24:37 +0900

    imageコンポーネント

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Link from 'next/link';
 import Layout from '../components/Layout';
+import Image from '../static/Image';
 
 export default () =>(
   <Layout header="Next" title="Top page.">
     <p>Welcome to next.js:</p>
+    <Image fname="image.png" size="100" />
     <hr />
     <Link href="/other">
       <button>Go to Other page &gt;&gt;</button>
